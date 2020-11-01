ec084a9e00360ea0f91fdcc5fa70759f10e16305
Isaiah Ewing <50162411+Shaiai@users.noreply.github.com>
2020-10-31 19:36:04 -0500

    First Puzzles
    - The Robot puzzles work as you give an array where the robot can only move down or right. Can not pass through X's or go out of Bounds of the "walls" 
    - Sudoku Solver uses backtracking and recursion in order to build a fully solved sudoku board. The board can be modified to solve and sudoku board.
    -Both use back tracking and recursion

---
+++ PythonPuzzleSolvers/RobotPuzzle/main.py
@@ -0,0 +1,63 @@
+# Backtracking
+
+maze = [["S", ".", ".","."],
+        [".","x","x","."],
+        [".",".",".","x"],
+        ["x","x",".","E"]]
+
+
+def print_maze(maze):
+  for row in maze:
+    row_print = ""
+    for value in row:
+      row_print += value + " "
+    print(row_print)
+
+
+
+def solve_maze(maze):
+  if len(maze) < 1:
+    return None
+  if len(maze[0]) < 1:
+    return None
+  
+  return solve_maze_helper(maze, [], 0, 0)
+
+
+def solve_maze_helper(maze, solution, pos_row, pos_col):
+  # Get the Size of the maze both rows and columns
+  num_row = len(maze)
+  num_col = len(maze[0])
+
+  # Base cases   
+
+  # Robot is already home  
+  if pos_row == num_row - 1 and pos_col == num_col -1:
+    return solution
+  
+  # Is the robot out of bounds?
+  if pos_row >= num_row or pos_col >= num_col:
+    return None
+  
+  # Is robuddy on an obstacle?
+  if maze[pos_row][pos_col] == 'x':
+    return None
+
+  # Recursive case
+
+  #Try going right
+  solution.append("r")
+  solution_going_right = solve_maze_helper(maze,solution,pos_row, pos_col +1)
+  if solution_going_right is not None:
+    return solution_going_right
+  
+  # Going Right Doesn't Work, Backtrack
+  solution.pop()
+  solution.append("d")
+  solution_going_down = solve_maze_helper(maze,solution,pos_row +1, pos_col)
+  if solution_going_down is not None:
+    return solution_going_down
+  
+  # No solution, impossible, Backtrack
+  solution.pop()
+  return None
---
+++ PythonPuzzleSolvers/Sudoku Solver/main.py
@@ -0,0 +1,85 @@
+#Program that can solve Sudoku Boards using backtracking / recursion
+
+#The layout of the board
+board = [[5,3,0,0,7,0,0,0,0],
+         [6,0,0,1,9,5,0,0,0],
+         [0,9,8,0,0,0,0,6,0],
+         [8,0,0,0,6,0,0,0,3],
+         [4,0,0,8,0,3,0,0,1],
+         [7,0,0,0,2,0,0,0,6],
+         [0,6,0,0,0,0,2,8,0],
+         [0,0,0,4,1,9,0,0,5],
+         [0,0,0,0,8,0,0,7,9]]  
+
+#Function that will display the board
+def print_board(board):
+  if board is None:
+    print("No Solution")
+  for row in board:
+    row_print = ""
+    for value in row:
+      row_print += str(value) + " "
+    print(row_print)
+
+#Function to find the next Zero in the puzzle
+def find_zero(board):
+  empty_cell = [] 
+  for i in range(len(board)):
+    for j in range(len(board[0])):
+      
+      if board[i][j] == 0:
+       empty_cell.append(i)
+       empty_cell.append(j)
+       return empty_cell
+
+#Function to check if the current value entered is valid by seeing if it's already
+#In the corresponding row or column
+def is_valid(board, row, col, value):
+
+    for i in range(len(board)):
+      if board[row][i] == value:
+        return False
+    
+    for i in range(len(board)):
+      if board[i][col] == value:
+        return False
+    
+    x = (row//3)*3
+    y = (col//3)*3
+    for i in range(3):
+      for j in range(3):
+        if board[x+i][y+j] == value:
+          return False
+    return True     
+
+def solve(board):
+  #Find the Empty Cell
+  empty_cell = find_zero(board)
+
+  #If there is no empty cell you return the board, it is complete
+  if empty_cell is None:
+    return print_board(board)
+    
+  #Keep Track of the row and column of your empty cell
+  row = empty_cell[0]
+  col = empty_cell[1]
+  
+  #Try filling those with numbers from 1 to 9
+  for value in range(1,10):
+    if is_valid(board, row, col, value):
+      board[row][col] = value
+      solution = solve(board)
+      if solution is not None:
+        return solution
+      board[row][col] = 0
+  return None                                           
+
+
+
+#Test case print outs
+print(find_zero(board))
+print(is_valid(board, 0 , 2 , 1 ))
+print(is_valid(board, 0 , 2 , 7 ))
+print(is_valid(board, 0 , 2 , 6 ))
+print(is_valid(board, 3 , 5 , 2 ))
+solve(board)
