65ced118196b808cb19239f6dbe27cb3acb22340
Isaiah Ewing <50162411+Shaiai@users.noreply.github.com>
2020-11-04 14:07:53 -0600

    Find Subtree
    Find subtree
    
    Given 2 binary trees t and s, find if s has a n equal subtree in t, where the structure and the values are the same. Return True if it exists, otherwise return False.

---
+++ PythonPuzzleSolvers/Find Subtree/main.py
@@ -0,0 +1,35 @@
+"""
+Find subtree
+
+Given 2 binary trees t and s, find if s has a n equal subtree in t, where the structure and the values are the same. Return True if it exists, otherwise return False.
+
+"""
+
+class Node():
+  def __init__(self, value, left = None, right = None):
+    self.value = value
+    self.left = left
+    self.right = right
+ 
+def __repr__(self):
+  return f"(Value: {self.value} Left: {self.left} Right: {self.right})"
+
+
+def find_subtree(s,t):
+
+  def preorder_traversal(node):
+    if node is None:
+      return ''
+    return str(node.value) + '-' + preorder_traversal(node.left) + '-' + preorder_traversal(node.right)
+
+  return preorder_traversal(s) in preorder_traversal(t)
+
+
+
+t3 = Node(4, Node(3), Node(2))
+t2 = Node(5, Node(4), Node(-1))
+t = Node(1, t2, t3)
+
+s = Node(4, Node(3), Node(2))
+
+print(find_subtree(s,t))
